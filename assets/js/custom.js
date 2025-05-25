5b4cc29e727297e219c114d8eead8d7189edcd8b
Arnaud Mengus <44212923+isontheline@users.noreply.github.com>
2025-05-25 09:18:40 +0200

    Add cache busting to custom CSS loading

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,9 +17,17 @@ function loadGoatCounterViews() {
 }
 
 function loadCustomCSS() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+  const day = String(now.getDate()).padStart(2, '0');
+  const hour = String(now.getHours()).padStart(2, '0');
+  const cacheBuster = `${year}-${month}-${day}-${hour}`;
+
   const css = document.createElement('link');
   css.rel = 'stylesheet';
-  css.href = '/assets/css/custom.css';
+  // year-month-day-hour
+  css.href = '/assets/css/custom.css?_=' + cacheBuster;
   document.head.appendChild(css);
 }
 
